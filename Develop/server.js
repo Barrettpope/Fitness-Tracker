a2eace990c80e8affa684894cd75928378ce8bed
Barrett Pope <barrettpope94@gmail.com>
2020-10-07 11:38:37 -0400

    Added morgan dependency to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const Workout = require("./models/user.js");
+app.use(logger("dev"));
 
 app.use(express.urlencoded({
     extended: true
